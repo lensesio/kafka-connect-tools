00315e7b0b761348684f94875b17db8a2627e637
Rollulus <roelboel@xs4all.nl>
2016-04-15 10:08:18 +0200

    Isolate scalaj http client in HttpClient trait

--- src/main/scala/com/datamountaineer/connect/tools/KafkaConnectApi.scala
+++ src/main/scala/com/datamountaineer/connect/tools/KafkaConnectApi.scala
@@ -1,8 +1,11 @@
 package com.datamountaineer.connect.tools
 
-import scalaj.http.{HttpResponse, Http}
+import scalaj.http.{Http, BaseHttp, HttpResponse}
 import spray.json._
 import DefaultJsonProtocol._
+import scala.util.{Try, Success, Failure}
+
+import spray.http._
 
 case class ErrorMessage(error_code: Int, message: String)
 case class Task(connector: String, task: Int)
@@ -14,47 +17,60 @@ object MyJsonProtocol extends DefaultJsonProtocol {
   implicit val errormsg = jsonFormat2(ErrorMessage)
 }
 
-// http://docs.confluent.io/2.1.0-alpha1/connect/userguide.html#rest-interface
-class KafkaConnectApi(url: java.net.URI) {
-  val defaultHeaders = Seq("Accept" -> "application/json", "Content-Type" -> "application/json")
+trait HttpClient {
+  def request(url: java.net.URI, method: String, hdrs: Seq[(String, String)], reqBody: Option[String]): Try[(Int, Option[String])]
+}
 
-  private def non2xxException(resp: HttpResponse[String]): Exception = {
+object ScalajHttpClient extends HttpClient {
+  def request(url: java.net.URI, method: String, hdrs: Seq[(String, String)], reqBody: Option[String]): Try[(Int, Option[String])] = {
+    try {
+      val r = Http(url.toString).headers(hdrs)
+      (reqBody match {
+        case Some(body) => r.postData(body)
+        case None => r
+      }).method(method).asString match {
+        case resp if resp.code == 204 => Success((resp.code, None))
+        case resp => Success((resp.code, Some(resp.body)))
+      }
+    } catch {
+      case e: Throwable => Failure(e)
+    }
+  }
+}
+
+// http://docs.confluent.io/2.1.0-alpha1/connect/userguide.html#rest-interface
+class KafkaConnectApi(baseUrl: java.net.URI, httpClient: HttpClient = ScalajHttpClient) {
+  val headers = Seq("Accept" -> "application/json", "Content-Type" -> "application/json")
+
+  private def non2xxException(status: Int, respBody: Option[String]): Exception = {
     import MyJsonProtocol.errormsg
     // try to deserialize message from body
     try {
-      val msg = resp.body.parseJson.convertTo[ErrorMessage]
+      val msg = respBody.get.parseJson.convertTo[ErrorMessage]
       new Exception(s"${msg.message} (${msg.error_code})")
     } catch {
-      case _: Throwable => new Exception(s"${resp.code} ${resp.statusLine}")
+      case _: Throwable => new Exception(s"${status}")
     }
   }
 
-  //TODO: some day I know how to improve this
-  private def req[T: JsonReader](endpoint: String, method: String = "GET", data: String = null): T = {
-    val r = Http(url.resolve(endpoint).toString).headers(defaultHeaders)
-    (r match {
-      case _ if data != null => r.postData(data)
-      case _ => r
-    }).method(method).asString match {
-      case resp if resp.is2xx => resp.body.parseJson.convertTo[T]
-      case resp => throw non2xxException(resp)
-    }
-  }
-
-  private def voidReq(endpoint: String, method: String = "GET"): Unit = {
-    Http(url.resolve(endpoint).toString).method(method).headers(defaultHeaders).asString match {
-      case resp if resp.is2xx =>
-      case resp => throw non2xxException(resp)
+  // constructs url from endpoint and base, inserts headers, etc.
+  private def req[T: JsonReader](endpoint: String, method: String = "GET", data: String = null): Option[T] = {
+    httpClient.request(baseUrl.resolve(endpoint), method, headers, Option(data))
+    match {
+      case Success((status, respBody)) if (status >= 200 && status < 300) =>
+        if (respBody.isDefined) Some(respBody.get.parseJson.convertTo[T]) else None
+      case Success((status, respBody)) => throw non2xxException(status, respBody)
+      case Failure(e) => throw e
     }
   }
 
   def activeConnectorNames(): Seq[String] = {
-    req[List[String]]("/connectors")
+    req[List[String]]("/connectors").get
   }
 
   def connectorInfo(name: String): ConnectorInfo = {
     import MyJsonProtocol.connectorinfo
-    req[ConnectorInfo](s"/connectors/${name}")
+    req[ConnectorInfo](s"/connectors/${name}").get
   }
 
   def addConnector(name: String, config: String) : ConnectorInfo = {
@@ -63,15 +79,15 @@ class KafkaConnectApi(url: java.net.URI) {
       s"""{
          |  "name": "${name}",
          |  "config": ${config}
-         |}""".stripMargin)
+         |}""".stripMargin).get
   }
 
   def updateConnector(name: String, config: String) : ConnectorInfo = {
     import MyJsonProtocol.connectorinfo
-    req[ConnectorInfo](s"/connectors/${name}/config","PUT", config)
+    req[ConnectorInfo](s"/connectors/${name}/config","PUT", config).get
   }
 
   def delete(name: String) = {
-    voidReq(s"/connectors/${name}","DELETE")
+    req[Unit](s"/connectors/${name}","DELETE")
   }
 }
